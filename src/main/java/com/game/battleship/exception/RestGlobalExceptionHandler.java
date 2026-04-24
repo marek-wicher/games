7e0e42082ab3c9d5b043e0c35764b3319dec97f6
Marek Wicher <do_not_replay@github.com>
2026-04-24 13:26:15 +0200

    [feature] - minor fixes after AI code generation

--- src/main/java/com/game/battleship/exception/RestGlobalExceptionHandler.java
+++ src/main/java/com/game/battleship/exception/RestGlobalExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.game.battleship.exception;
 
 import com.game.battleship.dto.ErrorResponseDto;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import static com.game.battleship.util.Commons.ErrorCodes.PAGE_NOT_FOUND_ERROR;
